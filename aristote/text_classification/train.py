9a3224e06b4f3daaa5d1abd3b8cd73250094c71c
julien wuthrich <julien.wut@gmail.com>
2020-10-25 01:22:13 -0700

    remove rows limitation during training

--- aristote/text_classification/train.py
+++ aristote/text_classification/train.py
@@ -9,7 +9,7 @@ from aristote.tensorflow_helper.trainer_helper import TensorflowTrainer
 if __name__ == '__main__':
     dataset_path = os.path.join(DATASET_PATH, "sentiment.csv")
     architecture = [('LCNN', 512), ("GLOBAL_AVERAGE_POOL", 0), ("DROPOUT", 0.1), ('DENSE', 256)]
-    dataset = pd.read_csv(dataset_path, nrows=1000)
+    dataset = pd.read_csv(dataset_path)
     x_col, y_col, label_type, epochs, name = "feature", "single", "multi-class", 5, "sentiment"
 
     projects = {
